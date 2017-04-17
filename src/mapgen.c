4edf95a90e5942cbb52e4fdc6799e9c3f1a0aa8e
SoVerySour <sebi.f@tuta.io>
2017-04-17 13:06:13 +0300

    Safety

--- src/mapgen.c
+++ src/mapgen.c
@@ -3,7 +3,7 @@
 #include "headers/main.h"
 #include "headers/utils.h"
 
-#define COLS 60
+#define COLS 80
 #define ROWS 28
 #define NR_ROOMS 5
 
@@ -14,11 +14,10 @@ struct room {
 
 char level = 0;
 
-struct object *map;
+struct object map[COLS][ROWS];
 
 int check_rooms(struct room x, struct room y){
-	int low_x = x.x > y.x ? y.x : x.x;
-	int low_y = x.y > y.y ? y.y : x.y;
+	
 
 	return 0;
 }
@@ -31,22 +30,19 @@ void load_map(void){
 		return;
 	}
 
-	map = get_object();
-	struct object *current = map;
-
 	for ( int i = 0; i < COLS; i++ )
 		for ( int j = 0; j < ROWS; j++ ){
-			*current = Owall;
-			current->x = i;
-			current->y = j;
-			current->next = get_object();
-			current = current->next;
-		}
+			map[i][j] = Owall;
+			map[i][j].x = i;
+			map[i][j].y = j;
 
-	*current = Owall;
-	current->y = ROWS - 1;
-	current->x = COLS - 1;
-	current->next = 0;
+			if ( i == COLS - 1 && j == ROWS - 1 )
+				map[i][j].next = 0;
+			else if ( j == ROWS - 1 )
+				map[i][j].next = map[i+1];
+			else
+				map[i][j].next = map[i] + j+1;
+		}
 
 	generation:
 	for ( int i = 0; i < NR_ROOMS; i++ ){
@@ -60,8 +56,14 @@ void load_map(void){
 		for ( int j = 0; j < NR_ROOMS; j++ )
 			if ( check_rooms(rooms[i], rooms[j]) )
 				goto generation;
+	
+	for ( int i = 0; i < NR_ROOMS; i++ )
+		for ( int j = rooms[i].x; j <= rooms[i].x + rooms[i].w; j++ )
+			for ( int k = rooms[i].y; k <= rooms[i].y + rooms[i].h; k++ )
+				if ( j * k && j < COLS - 1 && k < ROWS - 1 )
+					map[j][k].id = Ofloor.id;
 }
 
 struct object *get_map(void){
-	return map;
+	return map[0];
 }
