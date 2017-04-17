98031a12741940a59b62bc1f8771aca6eebb8b6e
SoVerySour <sebi.f@tuta.io>
2017-04-17 16:33:27 +0300

    Safety

--- src/mapgen.c
+++ src/mapgen.c
@@ -5,7 +5,7 @@
 
 #define COLS 80
 #define ROWS 28
-#define NR_ROOMS 5
+#define NR_ROOMS 17
 
 struct room {
 	int x, y;
@@ -14,11 +14,25 @@ struct room {
 
 char level = 0;
 
-struct object map[COLS][ROWS];
+struct object map[ROWS][COLS];
 
 int check_rooms(struct room x, struct room y){
+	if ( x.x < 2 || ( x.x + x.w > COLS - 3 ) )
+		return 1;
 
-	return 0;
+	if ( y.x < 2 || ( y.x + y.w > COLS - 3 ) )
+		return 1;
+
+	if ( x.y < 2 || ( x.y + x.h > ROWS - 3 ) )
+		return 1;
+
+	if ( y.y < 2 || ( y.y + y.h > ROWS - 3 ) )
+		return 1;
+
+	if ( x.x == y.x && x.y == y.y )
+		return 1;
+
+	return 0; 
 }
 
 void load_map(void){
@@ -29,38 +43,36 @@ void load_map(void){
 		return;
 	}
 
-	for ( int i = 0; i < COLS; i++ )
-		for ( int j = 0; j < ROWS; j++ ){
+	for ( int i = 0; i < ROWS; i++ )
+		for ( int j = 0; j < COLS; j++ ){
 			map[i][j] = Owall;
-			map[i][j].x = i;
-			map[i][j].y = j;
+			map[i][j].x = j;
+			map[i][j].y = i;
 
-			if ( i == COLS - 1 && j == ROWS - 1 )
+			if ( i == ROWS - 1 && j == COLS - 1 )
 				map[i][j].next = 0;
-			else if ( j == ROWS - 1 )
+			else if ( j == COLS - 1 )
 				map[i][j].next = map[i+1];
-			else
-				map[i][j].next = map[i] + j+1;
+			else map[i][j].next = map[i] + j + 1;
 		}
 
 	generation:
 	for ( int i = 0; i < NR_ROOMS; i++ ){
+		rooms[i].w = get_rand(8) + 3;
 		rooms[i].h = get_rand(4) + 5;
-		rooms[i].w = get_rand(3) + 3;
-		rooms[i].x = get_rand(COLS) - 1;
-		rooms[i].y = get_rand(ROWS) - 1;
+		rooms[i].x = get_rand(COLS) - 4;
+		rooms[i].y = get_rand(ROWS) - 4;
 	}
 
 	for ( int i = 0; i < NR_ROOMS - 1; i++ )
 		for ( int j = i + 1; j < NR_ROOMS; j++ )
 			if ( check_rooms(rooms[i], rooms[j]) )
 				goto generation;
-	
+
 	for ( int i = 0; i < NR_ROOMS; i++ )
-		for ( int j = rooms[i].x; j <= rooms[i].x + rooms[i].w; j++ )
-			for ( int k = rooms[i].y; k <= rooms[i].y + rooms[i].h; k++ )
-				if ( j * k && j < COLS - 1 && k < ROWS - 1 )
-					map[j][k].id = Ofloor.id;
+		for ( int j = rooms[i].y; j < rooms[i].y + rooms[i].h; j++ )
+			for ( int k = rooms[i].x; k < rooms[i].x + rooms[i].w; k++ )
+				map[j][k].id = Ofloor.id;
 }
 
 struct object *get_map(void){
